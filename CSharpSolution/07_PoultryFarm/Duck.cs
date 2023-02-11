d02b0e9ba77ca099660c99661c203bc11413b562
Péter György Tóth <ptoth@users.noreply.github.com>
2023-02-11 19:19:57 +0100

    20230211
    PoultryFarm

---
+++ CSharpSolution/07_PoultryFarm/Duck.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_PoultryFarm
+{
+    internal class Duck : Bird
+    {
+        public string makeSound()
+        {
+            return "Quack-quack-quack";
+        }
+    }
+}
---
+++ CSharpSolution/07_PoultryFarm/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _07_PoultryFarm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Készítsünk programot, ami egy baromfiudvar-nyilvántartó lesz.
+            1, hozzunk létre osztályokat: csirke, pulyka, fácán,, kacsa, liba(legyen színük, nemük(F / L))
+            2, töltsük fel a baromfiudvart mindegyikből kettő legyen
+            3, legyen mindegyik állatnak egyedi hangja
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
